f7e4eb4c335ddf26fe1cf6ae2ec7a67da548c18e
Mateusz Tokarz <mateusztokarz81@gmail.com>
2026-04-30 09:24:38 +0200

    Models.py - dodane klasy i relacje: Lotniska, samoloty, loty, pracownicy, harmonogram_załogi

--- backend/models.py
+++ backend/models.py
@@ -25,7 +25,8 @@ class Loty(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     numer_lotu: Mapped[str] = mapped_column(String(10), unique=True)
 
-    # relacje:
+#####################################################################
+    # relacje: Loty, Samoloty i lotniska
 
     id_samolotu: Mapped[int] = mapped_column(ForeignKey("samoloty.id"))
     id_lotniska_wylotu: Mapped[int]= mapped_column(ForeignKey("lotniska.id"))
@@ -47,7 +48,28 @@ class Loty(Base):
     cena_bazowa: Mapped[float] = mapped_column(Numeric(10, 2))
     wolne_miejsca: Mapped[int] = mapped_column(Integer)
 
+##################################################################################
+
+
+class Pracownik(Base):
+    __tablename__ = "pracownicy"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    imie: Mapped[str] = mapped_column(String(50))
+    nazwisko: Mapped[str] = mapped_column(String(50))
+    stanowisko: Mapped[str] = mapped_column(String(50))
+    numer_licencji: Mapped[str] = mapped_column(String(50))
+
+class HarmonogramZalogi(Base):
+    __tablename__ = "harmonogram_zalogi"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id_lotu: Mapped[int] = mapped_column(ForeignKey("loty.id"))
+    id_pracownika: Mapped[int] = mapped_column(ForeignKey("pracownicy.id"))
 
 
 
-    # dokończ resztę relacji
+
+ # relacje do wyciągania obiektów z tabel: "Loty" oraz "Pracownik"
+    lot: Mapped["Loty"] = relationship()
+    pracownik: Mapped["Pracownik"] = relationship()
