ac8127d8114a65176c262281e1febea04e9baa34
Mateusz Tokarz <mateusztokarz81@gmail.com>
2026-05-14 12:17:16 +0200

    dodanie obsługi CORS i rejestracja routera autoryzacji w main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from database import engine, Base
-from routers.operacje import router as operacje_router
-from routers.sprzedaz import router as sprzedaz_router
-import models  # noqa: F401 - import potrzebny, żeby SQLAlchemy "zobaczył" modele
+
+from fastapi.middleware.cors import CORSMiddleware # połączenie z frontendem (CORS)
+
+# Import routerów z folderu routers/
+from routers.operacje import router as operacje_router # moduł operacji na danych (CRUD) - dodawanie, edycja, usuwanie, pobieranie danych z bazy
+from routers.sprzedaz import router as sprzedaz_router # moduł sprzedaży biletów
+from routers.autoryzacja import router as autoryzacja_router # logowanie
+
+import models  #import potrzebny, żeby SQLAlchemy "zobaczył" modele
  
+
 # Tworzymy tabele w bazie przy starcie aplikacji.
-# UWAGA: Autor artykułu używa Alembica do migracji i nie potrzebuje tej linii.
 # My nie używamy Alembica, więc create_all() generuje tabele za nas.
 Base.metadata.create_all(bind=engine)
  
@@ -14,27 +20,43 @@ app = FastAPI(
     description="System zarządzania liniami lotniczymi SzwagiAIR"
 )
  
+
+# Konfiguracja CORS (Cross-Origin Resource Sharing)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 # Podpięcie routerów - każdy moduł ma własny plik w folderze routers/
 app.include_router(operacje_router)
 app.include_router(sprzedaz_router)
+app.include_router(autoryzacja_router)  # podpiąłem logowanie
  
  
 
-# AKTYWACJA ŚRODOWISKA WIRTUALNEGO:
-# .\szwagiAIR - lokalizacja projektu
-# .\venv\Scripts\activate
 
 # ==========================================
 # URUCHAMIANIE
 # ==========================================
 
-# start serwera:
+# 1. AKTYWACJA ŚRODOWISKA WIRTUALNEGO (w głównym foldrze):
+# .\szwagiAIR - lokalizacja projektu
+
+# .\venv\Scripts\activate
+
+# 2. start serwera (folder backend):
 #   cd .\backend\
 #   docker compose up -d
 #   uvicorn main:app --reload
-#
+
+
 # adres: http://127.0.0.1:8000/docs
-#
+
+# 3. Zatrzymanie serwera:
 # stop serwera:
 #   ctrl + c
 #   docker compose down
